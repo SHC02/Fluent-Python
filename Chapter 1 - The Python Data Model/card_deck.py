58929c0c2afcea01f2522ef9b318faff99b0cc22
Sehyun Cho <sehyun02@protonmail.ch>
2023-03-08 21:16:28 +0900

    03-08-2023

---
+++ Chapter 1 - The Python Data Model/card_deck.py
@@ -0,0 +1,28 @@
+'''
+demonstrates the power of implementing just two special methods,
+ __getitem__ and __len__.
+'''
+import collections
+from random import choice
+
+Card = collections.namedtuple('Card', ['rank', 'suit'])
+
+class FrenchDeck:
+    ranks = [str(i) for i in range(2, 11)] + list('JQKA')
+    suits = 'spades diamonds clubs hearts'.split()
+
+    def __init__(self):
+        self._cards = [Card(rank, suit) for suit in self.suits
+        for rank in self.ranks]
+    def __len__(self):
+        return len(self._cards)
+    def __getitem__(self, position):
+        return self._cards[position]
+
+beer_card = Card('7', 'diamonds')
+print(beer_card)
+
+deck = FrenchDeck()
+print(len(deck))
+
+print(choice(deck))
